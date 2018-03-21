0a4a0ecc237b9d57b4e73e93c65ac701c2703365
Ramón Maraboto <ramonturner@hotmail.com>
2018-03-20 23:12:36 -0600

    Add files via upload

---
+++ Assets/Alex/PasarNivelScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PasarNivelScript : MonoBehaviour
+{
+    public static PasarNivelScript script;
+    public GUIStyle myGUIStyle;
+
+    string mensaje;
+
+    void Awake()
+    {
+        script = this;
+        myGUIStyle.fontSize = 40;
+
+        mensaje = "";
+
+
+    }
+
+    public void GanoPerd(bool gano, string nivel)
+    {
+        StartCoroutine(IEnumNivel(gano, nivel));
+    }
+
+
+    public IEnumerator IEnumNivel(bool gano, string nivel)
+    {
+
+        if (gano)
+        {
+            PlayerPrefs.SetInt(nivel, 1);
+            mensaje = "Felicidades! Nivel Completado!";
+        }
+        else
+        {
+            mensaje = "Nivel Fallido :( ¿Intentar de nuevo?";
+
+        }
+
+
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("cueva");
+
+    }
+
+
+    void OnGUI()
+    {
+        myGUIStyle.normal.textColor = Color.red;
+
+        GUI.Label(new Rect(60, 100, 100, 20), mensaje, myGUIStyle);
+
+           
+    }
+}
